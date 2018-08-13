90f307994e9adc60b12fb3e61b6c495961d9e37d
Danilo8 <danilodesenvolvedorweb@hotmail.com>
2018-08-13 15:47:51 -0300

    Navbar Criada

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * --------------------------------------------------------------------------
+     * Função que retorna a View INDEX
+     * --------------------------------------------------------------------------
+     */
+    public function index()
+    {
+        return view('index');
+    }
 }
